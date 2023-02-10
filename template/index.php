3d987ef46c57ca9ec4180a9e09c144e45e32d8af
lichengwu <lichengwu@foxmail.com>
2023-02-10 17:37:24 +0800

    不要置顶模板

--- template/index.php
+++ template/index.php
@@ -4,11 +4,7 @@
   <main class="content">
     <div class="article-list">
       {foreach $articles as $article}
-      {if $article.IsTop}
-      {template:post-istop}
-      {else}
       {template:post-multi}
-      {/if}
       {/foreach}
     </div>
     <div class="pagination">
